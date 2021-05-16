01e67d01e88ba5472b6337fb868bc0d6c167d88c
PriyaShrinithi <rps.24.2000@gmail.com>
2021-05-16 21:14:35 +0530

    Initial Commit

--- Main/App/urls.py
+++ Main/App/urls.py
@@ -5,6 +5,6 @@ urlpatterns = [
     path('',views.home,name='home'),
     path('members/',views.members,name='members'),
     path('events/',views.events,name='events'),
-    path('resources/',views.members,name='resources'),
-    path('blogs/',views.events,name='blogs'),
+    path('resources/',views.resources,name='resources'),
+    path('blogs/',views.blogs,name='blogs'),
 ]
